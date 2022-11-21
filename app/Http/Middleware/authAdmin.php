89a2fa5794f95184f9d1e1a265b16519f412469e
ariesetiadi-edu <ariesetiadi.edu@gmail.com>
2022-11-21 10:35:07 +0800

    Add original database

---
+++ app/Http/Middleware/authAdmin.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class authAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $to = $request->path();
+        session()->put('to', $to);
+
+        // Boleh masuk jika pengguna adalah seorang Pelapor
+        if (session('auth')) {
+            if (session('auth')->jenis_pengguna != 'Pelapor') {
+                return $next($request);
+            } else {
+                return redirect()->to('/login');
+            }
+        } else {
+            return redirect()->to('/login');
+        }
+    }
+}
